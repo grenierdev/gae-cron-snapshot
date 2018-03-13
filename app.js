3d235a772af439f5ae68c82f104afad5d8014d39
MIchael Grenier <michael.grenier@live.com>
2018-03-13 10:59:57 -0400

    Snapshot rotation

--- app.js
+++ app.js
@@ -1,16 +1,103 @@
 "use strict";
 
+const assert = require('assert');
+const moment = require('moment');
 const express = require('express');
 const app = express();
 
 const Compute = require('@google-cloud/compute');
 const compute = new Compute();
-// Disks : https://cloud.google.com/nodejs/docs/reference/compute/0.9.x/Compute#getDisksStream
-// Snaps : https://cloud.google.com/nodejs/docs/reference/compute/0.9.x/Compute#getSnapshotsStream
 
 app.get('/', (req, res) => {
 	res.status(404).end();
 });
+
+app.get('/cron/hourly', async (req, res) => {
+	if (typeof req.headers['x-appengine-cron'] === 'undefined' || req.headers['x-appengine-cron'] !== 'true') {
+		res.status(404).end();
+	} else {
+		res.status(200).type('text/plain');
+
+		await rotateBackup('hourly', 'HHmm', 24);
+
+		res.end();
+	}
+});
+
+app.get('/cron/daily', async (req, res) => {
+	if (typeof req.headers['x-appengine-cron'] === 'undefined' || req.headers['x-appengine-cron'] !== 'true') {
+		res.status(404).end();
+	} else {
+		res.status(200).type('text/plain');
+
+		await rotateBackup('daily', 'YYYYMMDD', 7);
+
+		res.end();
+	}
+});
+
+app.get('/cron/weekly', async (req, res) => {
+	if (typeof req.headers['x-appengine-cron'] === 'undefined' || req.headers['x-appengine-cron'] !== 'true') {
+		res.status(404).end();
+	} else {
+		res.status(200).type('text/plain');
+
+		await rotateBackup('weekly', 'YYYYWW', 4);
+
+		res.end();
+	}
+});
+
+app.get('/cron/monthly', async (req, res) => {
+	if (typeof req.headers['x-appengine-cron'] === 'undefined' || req.headers['x-appengine-cron'] !== 'true') {
+		res.status(404).end();
+	} else {
+		res.status(200).type('text/plain');
+
+		await rotateBackup('monthly', 'YYYYMM', 12);
+
+		res.end();
+	}
+});
+
+if (module === require.main) {
+	const server = app.listen(process.env.PORT || 8080, () => {
+		const port = server.address().port;
+		console.log(`App listening on port ${port}.`);
+	});
+}
+
+module.exports = app;
+
+async function rotateBackup(suffix, format, keep) {
+	assert(typeof suffix === 'string' && suffix != '');
+	assert(typeof format === 'string' && format != '');
+	assert(typeof keep === 'number' && keep >= 1);
+
+	// Fetch all disk for this project
+	const disks = await fetchDisks();
+	for (let disk of disks) {
+		// Fetch all snapshot for this disk and type
+		let snaps = await fetchSnapshots(`(sourceDiskId eq "${disk.metadata.id}") (name eq ^${disk.id}-${suffix}.*)`);
+
+		// Create new snapshot
+		const name = `${disk.id}-${suffix}-${moment().format(format)}`;
+		console.log(`Creating snapshot ${name}`);
+		const [snap, op, resp] = await disk.createSnapshot(name);
+
+		// Add snapshot to list
+		snaps.push(snap);
+
+		// Sort by date using the name
+		snaps = snaps.sort((a, b) => a.id.localeCompare(b.id)).reverse();
+
+		// Remove extra snapshots
+		for (let i = keep, l = snaps.length; i < l; ++i) {
+			console.log(`Deleting snapshot ${snaps[i].id}`);
+			const [op, resp] = await snaps[i].delete();
+		}
+	}
+}
 
 function fetchDisks() {
 	return new Promise((resolve, reject) => {
@@ -31,13 +118,15 @@ function fetchDisks() {
 	});
 }
 
-function fetchSnapshots(diskId) {
+function fetchSnapshots(filter) {
 	return new Promise((resolve, reject) => {
 		const snaps = [];
 
 		try {
-			compute.getSnapshotsStream({ filter: `sourceDiskId eq ${diskId}` })
-				.on('error', (err) => reject(err))
+			compute.getSnapshotsStream({ filter })
+				.on('error', (err) => {
+					reject(err);
+				})
 				.on('data', (snap) => {
 					snaps.push(snap);
 				})
@@ -49,34 +138,3 @@ function fetchSnapshots(diskId) {
 		}
 	});
 }
-
-app.get('/test', async (req, res) => {
-	console.log(`Executing cron ${req.params.name}`);
-
-	res.status(200).type('text/plain');
-
-	const disks = await fetchDisks();
-
-	for (let disk of disks) {
-		const snaps = await fetchSnapshots(disk.metadata.id);
-
-		res.write(`Disk ${disk.id} has ${snaps.length} snapshots : ${snaps.map(snap => snap.id).join(', ')}\n`);
-		// TODO disk.createSnapshot(...) : https://cloud.google.com/compute/docs/reference/rest/v1/disks/createSnapshot
-	}
-
-	res.end();
-});
-
-app.get('/cron/:name', (req, res) => {
-	console.log(`Executing cron ${req.params.name}`);
-	res.status(200).send(`Cron ${req.params.name}`);
-});
-
-if (module === require.main) {
-	const server = app.listen(process.env.PORT || 8080, () => {
-		const port = server.address().port;
-		console.log(`App listening on port ${port}.`);
-	});
-}
-
-module.exports = app;
